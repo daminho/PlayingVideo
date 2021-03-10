578c9507779ab13bd77499f000618c07e9faf454
daminho <dangminhhoangdz@gmail.com>
2021-03-10 21:12:21 +0700

    testing

---
+++ main.js
@@ -0,0 +1,12 @@
+changeLink(link){
+	var Link = document.getElementById('userLink').value();
+	firstPart = '<iframe width="560" height="315" src="https://www.youtube.com/embed/"';
+	secondPart= ' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+	var index;
+	var need = "";
+	for(index = 32; index<Link.length; index++){
+		need+=Link[index];
+	}
+	newLink = firstPart+need+secondPart;
+	document.getElementById('userLink').src = newLink
+}
